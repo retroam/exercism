9af407f5d4d60c4c6714d9cc9d391065cf26c447
Robert Amanfu <robertamanfu@Roberts-MacBook-Pro-2.local>
2025-02-02 08:49:10 -0800

    adding some stuff

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -9,10 +9,11 @@ of a module and its functions and/or classes.
 
 
 #TODO: define the 'EXPECTED_BAKE_TIME' constant below.
-
+PREPARATION_TIME = 3
+EXPECTED_BAKE_TIME = 1
 
 #TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -23,18 +24,26 @@ def bake_time_remaining():
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    pass
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
 #TODO: Define the 'preparation_time_in_minutes()' function below.
 # You might also consider defining a 'PREPARATION_TIME' constant.
 # You can do that on the line below the 'EXPECTED_BAKE_TIME' constant.
 # This will make it easier to do calculations.
-
+def preparation_time_in_minutes():
+    """Calculate the preparation time in minutes.
+    
+    """
+    pass
 
 
 #TODO: define the 'elapsed_time_in_minutes()' function below.
-
+def elapsed_time_in_minutes():
+    """Calculate the elapsed time in minutes.
+    
+    """
+    pass
 
 
 # TODO: Remember to go back and add docstrings to all your functions
